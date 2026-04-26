749d8ab6164c57047a7965b4548b1d0e6eaaf7ce
Abir Hasan <abirhasanrawvy@gmail.com>
2026-04-27 02:36:27 +0600

    HTTP Client.

--- server.js
+++ server.js
@@ -1,30 +1,52 @@
 ///Request-Response Model (HTTP Server);
 
-import http from "node:http"
+// const http =require("http");
 
-const server = http.createServer((req, res) => {
-    const {method, url, headers} = req;
-    if(method === "GET" && url === "/") {
+// const server = http.createServer((req, res) => {
+//     const {method, url} = req;
+//     if(method === "GET" && url === "/") {
 
-        res.writeHead(200, {"Content-type": "text/plain" });
-        res.end("Hello from Node HTTP Server\n");
+//         res.writeHead(200, {"Content-type": "text/plain" });
+//         res.end("Hello from Node HTTP Server\n");
 
-    }else if(method === "POST" && url === "/echo"){
+//     } else if(method === "POST" && url === "/echo"){
 
-        let body = "";
-        res.on('data', (chunk) => (body += chunk));
-        res.on("end", () => {
-          res.writeHead(201, { "content-type": "application/json" });
-          res.end(JSON.stringifyf({ youSent: body}));  
-        });
+//         let body = "";
+//         req.on("data", (chunk) => {
+//             console.log("Chunk Data ", chunk.toString());
+//             body += chunk});
+//         req.on("end", () => {
+//           console.log("Recieved Data: ", body);
+//           res.writeHead(201, { "content-type": "application/json" });
+//           res.end(JSON.stringify({ youSent: body }));  
+//         });
 
-    }else{
+//     } else {
 
-        res.writeHead(404, {"Content-type": "text/plain" });
-        res.end("Not Found\n");
-    }
-});
+//         res.writeHead(404, {"Content-type": "text/plain" });
+//         res.end("Not Found\n");
+//     }
+// });
 
-server.listen(5000, () => console.log('Server on 5000 port.'));
+// server.listen(5000, () => console.log('Server on 5000 port.'));
 
 ///Something new;
+
+
+///HTTP Client;
+const https =require("http");
+
+https.get("https://jsonplaceholder.typicode.com/todos", (res) => {
+    let data = "";
+    res.on("data", (d) => {
+        data += d;
+    })
+    res.on("end", () => {
+        console.log('Status', res.statusCode);
+        console.log("Body; ", JSON.stringify(data));
+    });
+})
+.on("error", (err) => {
+    console.error(err);
+});
+
