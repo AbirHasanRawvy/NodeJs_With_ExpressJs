cba12c4e8fee30a9cc0a3955d5f1bedd1634188b
Abir Hasan <abirhasanrawvy@gmail.com>
2026-05-03 01:13:17 +0600

    Another CRUD.

--- class4.js
+++ class4.js
@@ -1,3 +1,45 @@
 ///
 const http = require("http");
-const fs = require("fs");
+const fs = require("fs").promises;
+const path = require("path");
+const url = require("url");
+
+
+class FileServer {
+
+    constructor(port){
+        this.port = port;
+        this.server = null;
+    }
+
+    async handleRequest(req, res){
+        const parsedUrl = url.parse(req.url, true);
+        const pathname = parsedUrl.pathname;
+        const method = parsedUrl.method;
+
+        //cors for frontend;
+        res.setHeader("Access-Control-Allow-Origin");
+        res.setHeader("Access-Control-Allow-Method", "GET,PUT,POST,DELETE");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+        if(method === "OPTIONS"){
+            res.writeHead(200);
+            return res.end();
+        }
+
+        try{
+
+            if(method === "GET" && pathname === "/"){
+                
+            }
+
+        }catch(error){
+            console.error(res, 500, {
+                error: "Server Error."
+            })
+        }
+
+    }
+}
+
+const server = new FileServer(3000)
