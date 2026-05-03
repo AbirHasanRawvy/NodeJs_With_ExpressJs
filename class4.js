026fcbc47a6b6f526cf90cc94a746d5b61c071eb
Abir Hasan <abirhasanrawvy@gmail.com>
2026-05-04 00:55:27 +0600

    Add files via upload

--- class4.js
+++ class4.js
@@ -34,6 +34,18 @@ class FileServer {
                 const html = await fs.readFile("index.html", "utf-8");
                 res.writeHead(200, {"Content-Type": "text/html"});
                 res.end(html);
+            }else if(method === "GET" && pathname === "/files"){
+                await this.listFiles(res);
+            }else if(method === "GET" && pathname === "/file/"){
+                const fileName = pathname.substring(6);
+                await this.readFile(res, fileName);
+            }else if(method === "POST" && pathname === "/file"){
+                await this.createFile(req, res);
+            }else if(method === "DELETE" && pathname.startsWith('/file/')){
+                const fileName = pathname.substring(6);
+                await this.deleteFile(res, fileName);
+            }else{
+                this.sendResponse(res, 404, {error: "Not Found"});
             }
 
         }catch(error){
@@ -47,6 +59,13 @@ class FileServer {
     async listFiles(res) {
         try{
             const files = await fs.readdir("./");
+            const textFiles = files.filter(file => file.extname(file)===".txt")
+            this.sendResponse(res, 200, textFiles);
+        }catch(error){
+            console.error(error);
+            this.Response(res, 500, {
+                error: "Server Error"
+            })
         }
     } 
 }
